dfd6226e6317d6652c6361f93cbc229823724c64
James Donaldson <jamdon@uw.edu>
2016-05-02 17:57:34 -0700

    add project files

---
+++ booklist.js
@@ -0,0 +1,129 @@
+var bookList = [
+  {
+    _id: 0,
+    title: 'Wool',
+    author: 'Hugh Howey',
+    genre: 'Science Fiction',
+    read: false
+  },
+  {
+    _id: 1,
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    genre: 'Fiction',
+    read: false
+  },
+  {
+    _id: 2,
+    title: 'Dune',
+    author: 'Frank Herbert',
+    genre: 'Science Fiction',
+    read: false
+  },
+  {
+    _id: 3,
+    title: 'The Fall of the Ottomans',
+    author: 'Eugene Rogan',
+    genre: 'History',
+    read: false
+  },
+  {
+    _id: 4,
+    title: 'Empire Falls',
+    author: 'Richard Russo',
+    genre: 'Fiction',
+    read: false
+  },
+  {
+    _id: 5,
+    title: 'Shift',
+    author: 'Hugh Howey',
+    genre: 'Science Fiction',
+    read: false
+  },
+  {
+    _id: 6,
+    title: 'Truman',
+    author: 'David McCullough',
+    genre: 'History',
+    read: false
+  },
+  {
+    _id: 7,
+    title: 'The Amazing Adventures of Kavalier and Klay',
+    author: 'Michael Chabon',
+    genre: 'Fiction',
+    read: false
+  },
+  {
+    _id: 8,
+    title: 'Ulysses',
+    author: 'James Joyce',
+    genre: 'Fiction',
+    read: false
+  },
+  {
+    _id: 9,
+    title: 'The Hobbit',
+    author: 'J.R. Tolkien',
+    genre: 'Fantasy',
+    read: false
+  }
+];
+
+var idManager = require('./id-manager');
+
+idManager.setIds(bookList);
+
+function getBooks() {
+  return bookList;
+}
+
+function getBook(id) {
+  id = parseInt(id, 10);
+
+  return bookList.find(function (book) {
+    return book._id === id;
+  });
+}
+
+function addBook(book) {
+  if (book) {
+    book.read = false;
+    book._id = idManager.getId();
+    bookList.push(book);
+  }
+}
+
+function removeBook(bookId) {
+  bookId = parseInt(bookId, 10);
+
+  var index = bookList.findIndex(function (book) {
+    return book._id === bookId;
+  });
+
+  bookList.splice(index, 1);
+}
+
+function updateBook(bookUpdate) {
+  bookId = parseInt(bookUpdate._id, 10);
+
+  var index = bookList.findIndex(function (book) {
+    return book._id === bookId;
+  });
+
+  bookList[index].title = bookUpdate.title;
+  bookList[index].author = bookUpdate.author;
+  bookList[index].genre = bookUpdate.genre;
+  bookList[index].read = bookUpdate.read;
+
+  return bookList[index];
+}
+
+module.exports = {
+  getBooks: getBooks,
+  getBook: getBook,
+  addBook: addBook,
+  removeBook: removeBook,
+  updateBook: updateBook
+};
---
+++ id-manager.js
@@ -0,0 +1,33 @@
+var lastId;
+
+function setIds(data) {
+  var ids = [];
+
+  if (Array.isArray(data)) {
+    ids = data.map(function (item) {
+      return parseInt(item._id, 10);
+    });
+  }
+  ids.sort();
+  if (ids.length) {
+    lastId = ids[ids.length - 1];
+    if (isNaN(lastId)) {
+      lastId = 0;
+    }
+  } else {
+    lastId = -1;
+  }
+}
+
+function getId() {
+  if (lastId === undefined) {
+    setIds();
+  }
+
+  return lastId + 1;
+}
+
+module.exports = {
+  setIds: setIds,
+  getId: getId
+};
